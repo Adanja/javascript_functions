82db8aca75b59cfe93764a5a42012fcf5ed3ff4e
Adanja <adanja-s@hotmail.com>
2021-04-10 16:10:39 +0200

    Uitwerken van functions.js en functions-with-parameters.js

--- functions.js
+++ functions.js
@@ -6,28 +6,44 @@ function morningGreeter() {
   return "Goedemorgen";
 }
 
-const greeting = morningGreeter();
+const greeting = morningGreeter(); // Waarom zou je de function is een const zetten als het zonder ook werkt??
 
 console.log(greeting);
 
 /* Opdracht 1 */
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
+function getSchool() {
+  return "Novi Hogeschool";}
 
+console.log(getSchool());
 
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+function compliment () {
+  return "Lekker bezig met die functies, Adanja! (Een anagram voor Jan met wat extra letters)";
+}
 
+console.log(compliment());
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
+function  getGrades () {
+  return [8, 8.5, 6, 7];
+}
 
-
+console.log(getGrades());
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails (firstName, lastName) {
+  return "firstname: " + firstName + " " + "lastName: " + lastName;
+}
+const firstStudent = getDetails("Adanja", "Spronk");
+console.log(firstStudent);
